5b4441a65643898077374e3ff81dbebce8fdfb55
gayathridoredla <gayathridoredla982@gmail.com>
2019-03-01 11:04:16 +0530

    LOG-udacity

---
+++ LAnysis_Ud.py
@@ -0,0 +1,94 @@
+"Log-Analysis Project"
+import psycopg2
+
+
+def cct_dbse():
+    """Connects to the news database and return a database cursor."""
+    try:
+        dtbse = psycopg2.connect("dbname=news")
+        cors1 = dtbse.cursor()
+    except:
+        print("Failed to connect to the PostgreSQL database.")
+        return None
+    else:
+        return cors1
+
+
+def mt_pp_te_aes(db1a_cors1):
+    """Query and print out the 3 most popular articles.
+    Args:
+        db_cursor: psycopg2 PostgreSQL database cursor object.
+    """
+    qy_128 = """
+            SELECT title, views FROM lanysis_ales INNER JOIN articles ON
+    articles.slug = lanysis_ales.slug ORDER BY views desc LIMIT 3;
+    """
+    db1a_cors1.execute(qy_128)
+    rsg_12 = db1a_cors1.fetchall()
+    print(" a).Three Most Popular Articles Of All Time??")
+    print(" ========================================")
+
+    for rsg_12 in rsg_12:
+        print(' --{title}  ------)  {count} views'
+              .format(title=rsg_12[0], count=rsg_12[1]))
+        print("\n")    
+    return
+
+
+def m_ppar_ators(db1a_cors1):
+    """Query and print out the most popular authors.
+    Args:
+        db_cursor: psycopg2 PostgreSQL database cursor object.
+    """
+    qe_23y = """
+            SELECT laors_name.name AS author,
+    sum(lanysis_ales.views) AS views FROM lanysis_ales INNER JOIN laors_name
+    ON laors_name.slug=lanysis_ales.slug
+    GROUP BY laors_name.name ORDER BY views desc limit 4;
+    """
+    db1a_cors1.execute(qe_23y)
+    rsg_12 = db1a_cors1.fetchall()
+
+    print(" b).Most Popular Authors Of All Time??")
+    print(" =================================")
+
+    for rsg_12 in rsg_12:
+        print(' --{author}  ------)  {count} views'
+              .format(author=rsg_12[0], count=rsg_12[1]))
+        print("\n")    
+    return
+
+
+def ds_ger_thn_eros(db1a_cors1):
+    """Query and print out days where the error rate is greater than 1%.
+    Args:
+        db_cursor: psycopg2 PostgreSQL database cursor object.
+    """
+    qu_a1x = """
+            SELECT leror_fail.date ,(
+            leror_fail.count*100.00 / lanysis_total.count) AS
+    percentage FROM leror_fail INNER JOIN lanysis_total
+    ON leror_fail.date = lanysis_total.date
+    AND (leror_fail.count*100.00 / lanysis_total.count) >1
+    ORDER BY (leror_fail.count*100.00 /lanysis_total.count) desc;
+    """
+    db1a_cors1.execute(qu_a1x)
+    rsg_12 = db1a_cors1.fetchall()
+
+    print(" c).Days With Greater Than 1% Errors??")
+    print(" =================================")
+
+    for rsg_12 in rsg_12:
+        print(' --{date:%B %d, %Y}  ------)  {error_rate:.1f}% errors'.format(
+            date=rsg_12[0],
+            error_rate=rsg_12[1]))
+        print("\n")
+        print(" **success** ")   
+    return
+if __name__ == "__main__":
+    CORS1 = cct_dbse()
+    if CORS1:
+        mt_pp_te_aes(CORS1)
+        m_ppar_ators(CORS1)
+        ds_ger_thn_eros(CORS1)
+        CORS1.close()
--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# LOG-udacity
-about the articles,titles,views are displayed
+# Project 3: Log-Analysis Project
+# By Doredla Gayathri
+
+Log-Analysis Project
+
+a). What it is and does??
+
+A Reporting page that prints out reports in a plain text format based on the data in the database.This reporting tool is a python program using the `psycopg2` module to connect to the database.
+
+b). Project content
+
+This project consists for the following files are:
+
+1) LAnysis_Ud.py - main file to run this Log Analysis Reporting tool
+2) README.md - Instructions to install this reporting tool
+3) newsdata.sql - database file
+4) OutPut.JPG
+
+c). Required Tools
+
+1. Python
+2. Vagrant
+3. VirtualBox
+
+d). Installation
+
+There are some dependancies and a few instructions on how to run the application.
+
+e). Dependencies
+
+1. [Vagrant](https://www.vagrantup.com/)
+2. [Udacity Vagrantfile](https://github.com/udacity/fullstack-nanodegree-vm)
+3. [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+
+f). How to Install
+1. Install Vagrant & VirtualBox
+2. Clone the Udacity Vagrantfile
+3. Go to Vagrant directory and either clone this repo or download and place zip here
+3. Launch the Vagrant VM (`vagrant up`)
+4. Log into Vagrant VM (`vagrant ssh`)
+5. Navigate to `cd /vagrant` as instructed in terminal
+
+g). How to Run Project
+
+Download the project zip file to you computer and unzip the file then place inside `vagrant/log-analysis`.
+
+  1. Launch the Vagrant VM inside Vagrant sub-directory in the downloaded fullstack-nanodegree-vm repository using command:
+  
+  ---
+    $ vagrant up
+  ---
+  2. Then Log into this using command:
+  
+  ---
+    $ vagrant ssh
+  ---
+  3. download database from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip).
+
+  4. Unzip this file after downloading it. The file inside is called newsdata.sql.
+
+  5. Copy the newsdata.sql file and place inside `vagrant/LAnysis_Ud.py`
+
+
+  6. In terminal Change directory to `vagrant/LAnysis_Ud.py` and look around with ls.
+
+  7. Load the data in local database using the command:
+
+  ---
+    $psql -d news -f views.sql
+    $ psql -d news -f newsdata.sql
+  ---
+   8. Run newsdata.py using:
+  ---
+    $ python newsdata.py
+  ---
+    $python LAnysis_Ud.py
+  Note: queries will take sometime to execute 
+
+
+h). Miscellaneous
+
+This README document is based on a template suggested by PhilipCoach. 
